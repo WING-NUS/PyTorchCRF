aa4012e7e5a26f9db36b894e791ea7b65800d63d
Yuan Chuan Kee <yuanchuan@u.nus.edu>
2018-12-14 17:20:00 +0800

    Specify PyTorch 1.0 as minimum requirement

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
 
     # Project uses reStructuredText, so ensure that the docutils get
     # installed or upgraded on the target machine
-    install_requires=['torch>=0.4.1'],
+    install_requires=['torch>=1.0.0'],
 
     package_data={
         # If any package contains *.txt or *.rst files, include them:
